5c93411ae0ae1314432fd99c4320484d2018ff06
ersin-ertan <ersin_the_ertan@hotmail.com>
2015-08-13 21:35:06 -0400

    Added: create actionbar method

--- app/src/main/java/com/nullcognition/udacityandroiddesignfordevelopers/MainActivity.java
+++ app/src/main/java/com/nullcognition/udacityandroiddesignfordevelopers/MainActivity.java
@@ -11,21 +11,27 @@ public class MainActivity extends AppCompatActivity{
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
+
+//		addFrameLayout();
+		selfMadeFloatingActionBar();
+	}
+
+	private void selfMadeFloatingActionBar(){
 		setContentView(R.layout.activity_main);
 
-		addFrameLayout();
 	}
 
 	private void addFrameLayout(){
+		setContentView(R.layout.activity_main);
 
 		for(int i = 0; i < 100; i++){
 
 			LinearLayout ll = (LinearLayout) findViewById(R.id.linearLayout);
 
 			FrameLayout fl = new FrameLayout(this);
-			fl.setElevation(i*3);
-			fl.setBackgroundColor(Color.argb(255, 255,255,255));
-			LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, 1+i);
+			fl.setElevation(i * 3);
+			fl.setBackgroundColor(Color.argb(255, 255, 255, 255));
+			LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, 1 + i);
 
 			ll.addView(fl, param);
 		}
